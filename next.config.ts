5fe690d015f5aee0e3a9c44319bfea1283e1dbae
affmis <145268243+affmis@users.noreply.github.com>
2025-02-01 13:22:30 +0200

    initial

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output:'export',
+  basePath: "/nextjs-github-pages",
 };
 
 export default nextConfig;
