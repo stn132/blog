ed09271cda11cfe9a925c36f7f7b36e94aecd7eb
affmis <145268243+affmis@users.noreply.github.com>
2025-02-14 14:46:12 +0200

    add images

---
+++ src/app/intro.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Intro: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Intro Page</h1>
+            <p>This is the introduction page of the blog.</p>
+        </div>
+    );
+};
+
+export default Intro;
